b808d171bcee70b103f356a0510e93fa2138aecd
Kaiteng Huang <huangkaiteng@gmail.com>
2011-12-09 15:48:14 +0800

    完成除了评论以外的ui

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -8,6 +8,7 @@ class MomentsController < ApplicationController
     end
     @moment = Moment.fetch(params[:id], :activity_id => params[:activity_id], :page => params[:page])
     @moment['current_page'] = 0 if @moment['current_page'].to_i == -1 # 如果没有找到activity，后台返回当前页数是-1  
+    puts @moment
   end
   
 
